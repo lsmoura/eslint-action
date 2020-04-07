fb7d908a921f676e5f4bc138e49c650d347a8db2
Sergio Moura <sergio@moura.ca>
2020-04-07 15:31:17 -0400

    create check

---
+++ src/request.js
@@ -0,0 +1,32 @@
+const https = require('https');
+
+function request(url, options) {
+  return new Promise((accept, reject) => {
+    const req = https
+      .request(url, options, res => {
+        let data = '';
+        res.on('data', chunk => {
+          data += chunk
+        });
+        res.on('end', () => {
+          if (res.statusCode >= 400) {
+            const err = new Error(`Received status code ${res.statusCode}`)
+            err.response = res
+            err.data = data
+            reject(err)
+          } else {
+            accept({ res, data: JSON.parse(data) })
+          }
+        });
+      })
+      .on('error', reject);
+    
+    if (options.body) {
+      req.end(JSON.stringify(options.body));
+    } else {
+      req.end();
+    }
+  });
+}
+
+module.exports = request;
--- src/run.js
+++ src/run.js
@@ -1,8 +1,39 @@
 const eslint = require('./eslint');
+const request = require('./request');
 
-console.log('Hello world from node');
+const { GITHUB_SHA, GITHUB_TOKEN, GITHUB_EVENT_PATH } = process.env;
+const event = require(GITHUB_EVENT_PATH);
 
-function run() {
+const CHECK_NAME = 'eslint';
+const OWNER = event.repository.owner.login;
+const REPO = event.repository.name;
+
+function createCheck() {
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/vnd.github.antiope-preview+json',
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+    'User-Agent': 'eslint-action',
+  };
+
+  const body = {
+    name: CHECK_NAME,
+    head_sha: GITHUB_SHA,
+    status: 'in_progress',
+    started_at: new Date(),
+  };
+
+  return request(`https://api.github.com/repos/${OWNER}/${REPO}/check-runs`, {
+    method: 'POST',
+    headers,
+    body
+  }).then(response => response.data);
+}
+
+
+async function run() {
+  const checkResponse = await createCheck();
+  console.log(checkResponse);
   eslint();
 }
 
